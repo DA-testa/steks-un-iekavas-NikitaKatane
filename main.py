9454e926d085b711aae8cfbd173939988afd2486
NikitaKatane <125819690+NikitaKatane@users.noreply.github.com>
2023-02-19 20:48:08 +0000

    Nikita Katane 221RDC055

--- main.py
+++ main.py
@@ -9,23 +9,39 @@ def are_matching(left, right):
     return (left + right) in ["()", "[]", "{}"]
 
 
-def find_mismatch(text):
+def find_mismatch(text) :
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            # Process opening bracket, write your code here
-            pass
+             opening_brackets_stack.append(Bracket(next,i+1))
 
         if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
+            if not are_matching(opening_brackets_stack [-1]. char , next):
+                return  i + 1
+            if not opening_brackets_stack :
+                return i + 1
+            opening_brackets_stack.pop()
+    if opening_brackets_stack :
+        return opening_brackets_stack [-1].position
+    else : 
+        return "Success"
 
 
-def main():
-    text = input()
+def main() :
+    do= input("I" or "F")
+    if "I"  in do :
+        text = input()
     mismatch = find_mismatch(text)
-    # Printing answer, write your code here
-
+    if mismatch == "Success" :
+        print ("Success")
+    elif "F" in do :
+        name = input("Enter name :")
+    with open(name, "r", encoding = "encond") as file :
+            text = file.read()
+            mismatch = find_mismatch(text)
+            if mismatch == "Success" :
+                print ("Success")
+            else : print(mismatch)
 
 if __name__ == "__main__":
     main()
